73ecfea64a764c4d17806504ced71d91ccd9c22b
verzac <benjamin.tanone@gmail.com>
2018-10-16 18:19:28 +1100

    updated mailservice to use spring boot's autowired javamailsender

--- src/main/java/com/benjamintanone/aboutapi/services/MailService.java
+++ src/main/java/com/benjamintanone/aboutapi/services/MailService.java
@@ -3,6 +3,7 @@ package com.benjamintanone.aboutapi.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
@@ -12,31 +13,18 @@ import java.util.Properties;
 @Service
 public class MailService {
 
-    @Value("${mail.user}")
-    private String userName;
-
-    @Value("${mail.password}")
-    private String password;
-
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-
-        mailSender.setUsername(userName);
-        mailSender.setPassword(password);
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
-        return mailSender;
-    }
+    @Autowired
+    private JavaMailSender mailSender;
 
     public MailService() {
 
+    }
+
+    public void sendMessageTo(String toEmail, String subject, String messageBody) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(messageBody);
+        mailSender.send(message);
     }
 }
