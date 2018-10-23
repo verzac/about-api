48611c923f620c5145b56a68a1bfc35ff765c2b4
verzac <benjamin.tanone@gmail.com>
2018-10-23 18:13:10 +1100

    added ordering for exception handlers

--- src/main/java/com/benjamintanone/aboutapi/exceptions/handlers/CustomExceptionHandler.java
+++ src/main/java/com/benjamintanone/aboutapi/exceptions/handlers/CustomExceptionHandler.java
@@ -3,21 +3,21 @@ package com.benjamintanone.aboutapi.exceptions.handlers;
 import com.benjamintanone.aboutapi.exceptions.BadRequestException;
 import com.benjamintanone.aboutapi.models.SimpleResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-@Order(Ordered.LOWEST_PRECEDENCE)
 @Slf4j
-public class GlobalExceptionHandler {
-    @ExceptionHandler(value = {RuntimeException.class})
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public SimpleResponse handleGeneralException(Exception e) {
-        log.error(e.getMessage(), e);
-        return SimpleResponse.builder().status(500).message("An unexpected error occurred.").build();
+public class CustomExceptionHandler {
+    @ExceptionHandler(value = {BadRequestException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public SimpleResponse handleBadRequestException(Exception exception) {
+        log.error(exception.getMessage(), exception);
+        return SimpleResponse.builder()
+                .message(exception.getMessage())
+                .status(400)
+                .build();
     }
 }
